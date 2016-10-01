0eba4b00e428a90de3367436eef3db5959c84736
kurukurupapa <gootarapapa@gmail.com>
2016-10-01 12:59:27 +0900

    ログファイル出力を考慮してみました。

--- Bat/Template.bat
+++ Bat/Template.bat
@@ -2,6 +2,7 @@
 rem Windowsバッチファイルのテンプレートです。
 
 set basedir=%~dp0
+set basename=%~n0
 set batname=%~n0%~x0
 set datestr=%DATE:/=%
 set timestrtmp=%TIME: =0%
@@ -9,33 +10,34 @@ set timestr=%timestrtmp:~0,2%%timestrtmp:~3,2%%timestrtmp:~6,2%
 set timestamp=%datestr%-%timestr%
 
 :INIT
-if "%1"=="" goto USAGE
-if "%1"=="/?" goto USAGE
-set /p input=開始してよろしいですか？ (y/n[y])
-if "%input%"=="" set input=y
-if not "%input%"=="y" goto EOF
+if "%1"=="/?" (
+  echo 使い方：%batname% [/?]
+  exit /b 0
+)
 
 :MAIN
 call :LOG 処理開始します。
 
 
 
-rem <<< ここに処理を書きます >>>
+rem ▼▼▼ここに処理を書きます
+echo basedir=%basedir%
+echo basename=%basename%
+echo batname=%batname%
+echo timestamp=%timestamp%
+echo 引数1=%1
+echo 引数2=%2
+rem ▲▲▲ここに処理を書きます
 
 
 
 :END
 call :LOG 正常終了です。
-pause
-exit /b 0
-
-:USAGE
-echo 使い方：%batname%
 exit /b 0
 
 :ERROR
-echo エラー終了です。
-exit /b -1
+echo 異常終了です。
+exit /b 1
 
 :LOG
 echo %DATE% %TIME% %basename% %1
---
+++ Bat/TemplateWrapper.bat
@@ -0,0 +1,18 @@
+@echo off
+rem Windowsバッチファイルを呼び出します。
+
+set basedir=%~dp0
+set basename=%~n0
+set batname=%~n0%~x0
+set mainname=%basename:Wrapper=%
+set logpath=%CD%\%mainname%.log
+
+set /p input=開始してよろしいですか？ (y/n[y])
+if "%input%"=="" set input=y
+if not "%input%"=="y" goto EOF
+
+echo ログ：%logpath%
+call "%basedir%\%mainname%.bat" %1 %2 %3 %4 %5 %6 %7 %8 %9 > "%logpath%" 2>&1
+
+pause
+exit /b 0
