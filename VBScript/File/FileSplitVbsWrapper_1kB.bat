c35ac458ed9f0b3bc02cbf2c3009faf2100f4e02
kurukurupapa <gootarapapa@gmail.com>
2016-12-11 14:53:36 +0900

    add

---
+++ VBScript/File/FileSplitVbsWrapper_1kB.bat
@@ -0,0 +1,26 @@
+@echo off
+rem VBScriptを呼び出します。
+
+set basedir=%~dp0
+set basename=%~n0
+set batname=%~n0%~x0
+set mainname=FileSplit
+set logpath=%CD%\%mainname%.log
+
+rem set /p input=開始してよろしいですか？ (y/n[y])
+rem if "%input%"=="" set input=y
+rem if not "%input%"=="y" exit /b 1
+
+echo ログ：%logpath%
+type nul > "%logpath%"
+if "%1"=="" (
+  cscript //Nologo "%basedir%\%mainname%.vbs" >> "%logpath%" 2>&1
+) else (
+  for %%a in (%*) do (
+    cscript //Nologo "%basedir%\%mainname%.vbs" 1024 %%a >> "%logpath%" 2>&1
+  )
+)
+type "%logpath%"
+
+rem pause
+exit /b 0
